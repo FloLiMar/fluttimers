a0c06e277ca2308acb09e517f35f7a510f093da3
Florence DAVID <florence.david88@gmail.com>
2024-01-30 20:58:14 +0100

    TIMER-03 add formatting to time wheel

--- lib/timerSelector/presentation/TimeWheelWidget.dart
+++ lib/timerSelector/presentation/TimeWheelWidget.dart
@@ -1,24 +1,33 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 import '../../common/widget/NumberListWheelWidget.dart';
 import '../domain/TimeParameter.dart';
 
 class TimeWheelWidget extends StatelessWidget {
   final TimeParameter timeParameter;
-  const TimeWheelWidget({super.key, required this.timeParameter});
 
+  const TimeWheelWidget({super.key, required this.timeParameter});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        NumberListWheelWidget(
-            onSelectedItemChangedCallback: timeParameter.addValue,
-            maxValue: timeParameter.maxValue),
-        Text(timeParameter.unit)
-      ],
+    final textStyle = Theme.of(context).textTheme;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          NumberListWheelWidget(
+              onSelectedItemChangedCallback: timeParameter.setValue,
+              maxValue: timeParameter.maxValue),
+          Container(
+              padding: const EdgeInsets.only(top: 14, left: 6),
+              child: Text(
+                timeParameter.unit,
+                style: textStyle.bodyLarge,
+              ))
+        ],
+      ),
     );
   }
-
 }
